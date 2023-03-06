4bb766e8f6bc74fbaadbb1a0902a91e7b00ef00e
Ali Aliyev <ali@aliev.me>
2023-03-06 23:29:50 +0400

    fix: linter issues

--- aioshutdown/_types.py
+++ aioshutdown/_types.py
@@ -1,6 +1,5 @@
 import asyncio
 from signal import Signals
-from typing import Callable, Coroutine, Any
-
+from typing import Any, Callable, Coroutine
 
 HandlerType = Callable[[asyncio.AbstractEventLoop, Signals], Coroutine[Any, Any, None]]
